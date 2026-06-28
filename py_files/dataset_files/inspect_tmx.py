09dd313f88391effb069c27223fd6d9814dea2a3
sandrapoer <sandrapoernbacher@gmail.com>
2026-06-28 19:10:37 +0000

    deleting files

--- py_files/dataset_files/inspect_tmx.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as ET
-
-tree = ET.parse("data/raw/tico19_en_fr.tmx")
-root = tree.getroot()
-
-# Count translation units
-translation_units = len(root.findall(".//{*}tu"))
-print("Number of translation units in TMX file:", translation_units)
